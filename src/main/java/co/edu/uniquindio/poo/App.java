16688873a825064f7ad49ce8abca74460798a366
Héctor Daniel Gonzalez Sánchez <hectord.gonzalezs@uqvirtual.edu.co>
2024-05-09 10:42:10 -0500

    Eliminacion ejemplos, inicio proyecto

--- src/main/java/co/edu/uniquindio/poo/App.java
+++ src/main/java/co/edu/uniquindio/poo/App.java
@@ -3,17 +3,7 @@ package co.edu.uniquindio.poo;
 public class App {
     public static void main(String[] args) {
 
-        Perro miPerro = new Perro("Albeiro", 7);
-        miPerro.emitirSonido();
-        miPerro.comer();
-
-        Gato miGato = new Gato("Misirili", 3);
-        miGato.emitirSonido();
-        miGato.comer();
-
-        Pato miPato = new Pato();
-        miPato.volar();
-        miPato.nadar();
+    System.out.println("Hola mundo");
 
     }
 }
--- src/main/java/co/edu/uniquindio/poo/Volador.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uniquindio.poo;
-
-public interface Volador {
-    
-    //Metodo volar() para una clase
-    public void volar();
-
-}
