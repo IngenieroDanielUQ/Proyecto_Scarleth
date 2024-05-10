1818fd2acd6dc6239e9bd93942541bf3625995d0
Héctor Daniel Gonzalez Sánchez <hectord.gonzalezs@uqvirtual.edu.co>
2024-05-10 17:45:30 -0500

    Actualización de clase padre

--- src/main/java/co/edu/uniquindio/poo/Automovil.java
+++ src/main/java/co/edu/uniquindio/poo/Automovil.java
@@ -2,16 +2,13 @@ package co.edu.uniquindio.poo;
 
 public class Automovil extends Objeto implements Bluetooth{
     private String traccion;
-    private boolean conexion;
 
 
     public Automovil(String nombre, String modelo, String traccion, boolean conexion) {
-        super(nombre, modelo);
+        super(nombre, modelo, conexion);
         this.traccion = traccion;
-        this.conexion = conexion;
     }
-
-
+    
     public String getTraccion() {
         System.out.println("Traccion del vehiculo: " + traccion);
         return traccion;
@@ -20,15 +17,6 @@ public class Automovil extends Objeto implements Bluetooth{
     public void setTraccion(String traccion) {
         this.traccion = traccion;
     }
-
-    public boolean isConexion() {
-        return conexion;
-    }
-
-    public void setConexion(boolean conexion) {
-        this.conexion = conexion;
-    }
-
     @Override
     public void conectarDispositivo() {
         System.out.println("Estado de conexion Bluetooth para " + nombre + ": "+(conexion ? "Dispositivo conectado" : "Dispositivo desconectado" + "\n"));
--- src/main/java/co/edu/uniquindio/poo/Objeto.java
+++ src/main/java/co/edu/uniquindio/poo/Objeto.java
@@ -4,26 +4,30 @@ package co.edu.uniquindio.poo;
 public class Objeto {
     protected  String nombre;
     protected  String modelo;
+    protected boolean conexion;
 
-    public Objeto(String nombre, String modelo) {
+    public Objeto(String nombre, String modelo, boolean conexion) {
         this.nombre = nombre;
         this.modelo = modelo;
+        this.conexion = conexion;
     }
-
+    public boolean isConexion() {
+        return conexion;
+    }
+    public void setConexion(boolean conexion) {
+        this.conexion = conexion;
+    }
     public String getNombre() {
         System.out.println("Nombre del Vehiculo: " + nombre);
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public String getModelo() {
         System.out.println("Modelo del Vehiculo: " + modelo);
         return modelo;
     }
-
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
